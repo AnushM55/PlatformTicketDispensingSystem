7b593e941a857b939c47f1fef2065b26a6fe82cd
AnushM55 <143617212+AnushM55@users.noreply.github.com>
2024-04-19 19:46:43 +0530

    initialise sample database connection

--- platformticketdispensingsystem/src/main/java/com/ptds/Main.java
+++ platformticketdispensingsystem/src/main/java/com/ptds/Main.java
@@ -1,7 +1,28 @@
 package com.ptds;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String strUrl = "jdbc:postgresql://localhost:5432/SampleDatabase";
+        String strUsr = "newadmin";
+        String PWD = "";
+        
+        try {
+            Connection conn = DriverManager.getConnection(strUrl, strUsr, PWD);
+            Statement stat = conn.createStatement();
+            ResultSet recst = stat.executeQuery("select * from SampleTable;");
+            System.out.println(recst.getMetaData());
+            conn.close();
+
+            
+        }
+    catch(Exception e){
+        System.err.println("error : "+e.getLocalizedMessage());
     }
+}
 }
